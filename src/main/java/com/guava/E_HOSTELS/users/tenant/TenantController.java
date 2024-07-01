67cedddffae716b779dd98cfa647e5e288ad65ef
oscar <oscar@kali.oscar.com>
2024-07-01 22:28:42 +0300

    modified all the notifications tab

--- src/main/java/com/guava/E_HOSTELS/users/tenant/TenantController.java
+++ src/main/java/com/guava/E_HOSTELS/users/tenant/TenantController.java
@@ -62,7 +62,7 @@ public class TenantController {
 
         Page<Building> buildings;
 
-        if (area == null || area.isEmpty() && maxDistance == null && maxPrice == null && buildingName == null) {
+        if ((area == null || area.isEmpty() )&& maxDistance == null && maxPrice == null && (buildingName == null || buildingName.isEmpty() )) {
             buildings = buildingRepository.findAllWithLandlord(pageable);
         } else {
             buildings = buildingService.searchBuildings(area, maxDistance, maxPrice, buildingName, pageable);
